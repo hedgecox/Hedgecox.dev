c16a40daa02f2e51d43f4f54a067c915811c05d8
hedgecox <luke.hedgecox@aol.com>
2020-02-03 14:11:21 +0000

    Bulb yellow glow, partial transparency

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled, { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
@@ -14,12 +14,36 @@ const Main = styled.main`
 `;
 
 const Bulb = styled.img`
+	max-width: 100%;
+`;
+
+const BulbContainer = styled.div`
+	--bulb-opacity: 0.6;
 	position: absolute;
+	top: 0;
+	right: 200px;
+	max-width: 650px;
+	z-index: 1;
+
+	&:after {
+		content: "";
+		width: 0;
+		height: 245px;
+		position: absolute;
+		top: 320px;
+		left: 327px;
+		box-shadow: 0px 67px 106px 39px rgba(251, 255, 0, var(--bulb-opacity));
+	}
 `;
 
 const App = () => {
 	const [Brightness, SetBrightness] = useState(50);
 	const BrightnessChange = e => SetBrightness(e.target.value);
+	const BulbContainerRef = useRef();
+
+	useEffect(() => {
+		BulbContainerRef.current.style.setProperty("--bulb-opacity", Brightness / 255);
+	}, [Brightness]);
 
 	return (
 		<>
@@ -28,13 +52,15 @@ const App = () => {
 				<input
 					type="range"
 					min={50}
-					max={255}
+					max={240}
 					value={Brightness}
 					onChange={BrightnessChange}
 				/>
 			</Main>
 
-			<Bulb src="/bulb.png" alt="Lightbulb" />
+			<BulbContainer ref={BulbContainerRef}>
+				<Bulb src="/bulb2.png" alt="Lightbulb" />
+			</BulbContainer>
 		</>
 	);
 };
